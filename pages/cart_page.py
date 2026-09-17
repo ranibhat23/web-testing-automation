f5988348446a3833af8fdb9699d272d57387b9e9
Rani Raviraj Bhat <z5556761@ad.unsw.edu.au>
2026-09-17 23:31:06 +1000

    Modified cart page

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -3,7 +3,6 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from pages.base_page import BasePage
 
-
 class CartPage(BasePage):
 
     CHECKOUT_BUTTON = (
@@ -28,7 +27,7 @@ class CartPage(BasePage):
         self.click(self.CHECKOUT_BUTTON)
 
         self.wait.until(
-            EC.visibility_of_element_located(
+            EC.presence_of_element_located(
                 (By.ID, "first-name")
             )
         )
